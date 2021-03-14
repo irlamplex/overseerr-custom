287208ac15c3f30d2d5e3d64b2f7ed07bda67357
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-03-14 19:36:56 -0400

    feat(notif): allow users to enable/disable specific agents

--- server/lib/notifications/agents/discord.ts
+++ server/lib/notifications/agents/discord.ts
@@ -218,7 +218,7 @@ class DiscordAgent
 
       if (
         payload.notifyUser &&
-        payload.notifyUser.settings?.enableNotifications &&
+        payload.notifyUser.settings?.enableDiscord &&
         payload.notifyUser.settings?.discordId
       ) {
         mentionedUsers.push(payload.notifyUser.settings.discordId);
