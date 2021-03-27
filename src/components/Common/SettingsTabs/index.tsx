nosaj72/overseerr

refactor(ui): add tabs to user notification settings

src/components/Common/SettingsTabs/index.tsx
@@ -1,0 +1,175 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useUser } from '../../../hooks/useUser';
+import { Permission, hasPermission } from '../../../../server/lib/permissions';
+
+export interface SettingsRoute {
+  text: string;
+  content?: React.ReactNode;
+  route: string;
+  regex: RegExp;
+  requiredPermission?: Permission | Permission[];
+  permissionType?: { type: 'and' | 'or' };
+  hidden?: boolean;
+}
+
+const SettingsLink: React.FC<{
+  tabType: 'default' | 'button';
+  currentPath: string;
+  route: string;
+  regex: RegExp;
+  hidden?: boolean;
+  isMobile?: boolean;
+}> = ({
+  children,
+  tabType,
+  currentPath,
+  route,
+  regex,
+  hidden = false,
+  isMobile = false,
+}) => {
+  if (hidden) {
+    return null;
+  }
+
+  if (isMobile) {
+    return <option value={route}>{children}</option>;
+  }
+
+  let linkClasses =
+    'whitespace-nowrap ml-8 first:ml-0 py-4 px-1 border-b-2 border-transparent font-medium text-sm leading-5';
+  let activeLinkColor =
+    'border-indigo-600 text-indigo-500 focus:outline-none focus:text-indigo-500 focus:border-indigo-500';
+  let inactiveLinkColor =
+    'border-transparent text-gray-500 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-4700 focus:border-gray-300';
+
+  if (tabType === 'button') {
+    linkClasses =
+      'whitespace-nowrap ml-8 first:ml-0 px-3 py-2 font-medium text-sm rounded-md';
+    activeLinkColor = 'bg-indigo-700';
+    inactiveLinkColor = 'bg-gray-800';
+  }
+
+  return (
+    <Link href={route}>
+      <a
+        className={`${linkClasses} ${
+          currentPath.match(regex) ? activeLinkColor : inactiveLinkColor
+        }`}
+        aria-current="page"
+      >
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+const SettingsTabs: React.FC<{
+  tabType?: 'default' | 'button';
+  settingsRoutes: SettingsRoute[];
+  defaultValue?: string;
+}> = ({ tabType = 'default', settingsRoutes, defaultValue }) => {
+  const router = useRouter();
+  const { user: currentUser } = useUser();
+
+  return (
+    <>
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a Tab
+        </label>
+        <select
+          onChange={(e) => {
+            router.push(e.target.value);
+          }}
+          onBlur={(e) => {
+            router.push(e.target.value);
+          }}
+          defaultValue={
+            defaultValue ??
+            settingsRoutes.find((route) => !!router.pathname.match(route.regex))
+              ?.route
+          }
+          aria-label="Selected Tab"
+        >
+          {settingsRoutes
+            .filter(
+              (route) =>
+                !route.hidden &&
+                (route.requiredPermission
+                  ? hasPermission(
+                      route.requiredPermission,
+                      currentUser?.permissions ?? 0,
+                      route.permissionType
+                    )
+                  : true)
+            )
+            .map((route, index) => (
+              <SettingsLink
+                tabType={tabType}
+                currentPath={router.pathname}
+                route={route.route}
+                regex={route.regex}
+                isMobile
+                key={`mobile-settings-link-${index}`}
+              >
+                {route.text}
+                {router.pathname}
+              </SettingsLink>
+            ))}
+        </select>
+      </div>
+      {tabType === 'button' ? (
+        <div className="hidden overflow-x-scroll overflow-y-hidden sm:block hide-scrollbar">
+          <nav className="flex space-x-4" aria-label="Tabs">
+            {settingsRoutes.map((route, index) => (
+              <SettingsLink
+                tabType={tabType}
+                currentPath={router.pathname}
+                route={route.route}
+                regex={route.regex}
+                key={`standard-settings-link-${index}`}
+              >
+                {route.content ?? route.text}
+              </SettingsLink>
+            ))}
+          </nav>
+        </div>
+      ) : (
+        <div className="hidden sm:block">
+          <div className="border-b border-gray-600">
+            <nav className="flex -mb-px">
+              {settingsRoutes
+                .filter(
+                  (route) =>
+                    !route.hidden &&
+                    (route.requiredPermission
+                      ? hasPermission(
+                          route.requiredPermission,
+                          currentUser?.permissions ?? 0,
+                          route.permissionType
+                        )
+                      : true)
+                )
+                .map((route, index) => (
+                  <SettingsLink
+                    tabType={tabType}
+                    currentPath={router.pathname}
+                    route={route.route}
+                    regex={route.regex}
+                    key={`standard-settings-link-${index}`}
+                  >
+                    {route.text}
+                  </SettingsLink>
+                ))}
+            </nav>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SettingsTabs;

src/components/UserProfile/UserSettings/UserNotificationSettings/UserNotificationsDiscord.tsx
@@ -1,0 +1,123 @@
+import React from 'react';
+import { Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import Button from '../../../Common/Button';
+import LoadingSpinner from '../../../Common/LoadingSpinner';
+import { defineMessages, useIntl } from 'react-intl';
+import axios from 'axios';
+import * as Yup from 'yup';
+import { useToasts } from 'react-toast-notifications';
+import { useUser } from '../../../../hooks/useUser';
+import { UserSettingsNotificationsResponse } from '../../../../../server/interfaces/api/userSettingsInterfaces';
+
+const messages = defineMessages({
+  save: 'Save Changes',
+  saving: 'Saving…',
+  discordsettingssaved: 'Discord notification settings saved successfully!',
+  discordsettingsfailed: 'Discord notification settings failed to save.',
+  discordId: 'User ID',
+  discordIdTip:
+    'The <FindDiscordIdLink>ID number</FindDiscordIdLink> for your user account',
+  validationDiscordId: 'You must provide a valid user ID',
+});
+
+const UserNotificationsDiscord: React.FC = () => {
+  const intl = useIntl();
+  const { addToast } = useToasts();
+  const router = useRouter();
+  const { user } = useUser({ id: Number(router.query.discordId) });
+  const { data, error, revalidate } = useSWR<UserSettingsNotificationsResponse>(
+    user ? `/api/v1/user/${user?.id}/settings/notifications` : null
+  );
+
+  const UserNotificationsDiscordSchema = Yup.object().shape({
+    discordId: Yup.string()
+      .nullable()
+      .matches(/^\d{17,18}$/, intl.formatMessage(messages.validationDiscordId)),
+  });
+
+  if (!data && !error) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        discordId: data?.discordId,
+      }}
+      validationSchema={UserNotificationsDiscordSchema}
+      onSubmit={async (values) => {
+        try {
+          await axios.post(`/api/v1/user/${user?.id}/settings/notifications`, {
+            discordId: values.discordId,
+          });
+          addToast(intl.formatMessage(messages.discordsettingssaved), {
+            appearance: 'success',
+            autoDismiss: true,
+          });
+        } catch (e) {
+          addToast(intl.formatMessage(messages.discordsettingsfailed), {
+            appearance: 'error',
+            autoDismiss: true,
+          });
+        } finally {
+          revalidate();
+        }
+      }}
+    >
+      {({ errors, touched, isSubmitting }) => {
+        return (
+          <Form className="section">
+            <div className="form-row">
+              <label htmlFor="discordId" className="text-label">
+                <span>{intl.formatMessage(messages.discordId)}</span>
+                <span className="label-tip">
+                  {intl.formatMessage(messages.discordIdTip, {
+                    FindDiscordIdLink: function FindDiscordIdLink(msg) {
+                      return (
+                        <a
+                          href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-gray-100 underline transition duration-300 hover:text-white"
+                        >
+                          {msg}
+                        </a>
+                      );
+                    },
+                  })}
+                </span>
+              </label>
+              <div className="form-input">
+                <div className="form-input-field">
+                  <Field id="discordId" name="discordId" type="text" />
+                </div>
+                {errors.discordId && touched.discordId && (
+                  <div className="error">{errors.discordId}</div>
+                )}
+              </div>
+            </div>
+            <div className="actions">
+              <div className="flex justify-end">
+                <span className="inline-flex ml-3 rounded-md shadow-sm">
+                  <Button
+                    buttonType="primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting
+                      ? intl.formatMessage(messages.saving)
+                      : intl.formatMessage(messages.save)}
+                  </Button>
+                </span>
+              </div>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default UserNotificationsDiscord;

irlamplex/overseerr-custom

src/components/Common/SettingsTabs/index.tsx
@@ -1,0 +1,175 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { hasPermission, Permission } from '../../../../server/lib/permissions';
+import { useUser } from '../../../hooks/useUser';
+
+export interface SettingsRoute {
+  text: string;
+  content?: React.ReactNode;
+  route: string;
+  regex: RegExp;
+  requiredPermission?: Permission | Permission[];
+  permissionType?: { type: 'and' | 'or' };
+  hidden?: boolean;
+}
+
+const SettingsLink: React.FC<{
+  tabType: 'default' | 'button';
+  currentPath: string;
+  route: string;
+  regex: RegExp;
+  hidden?: boolean;
+  isMobile?: boolean;
+}> = ({
+  children,
+  tabType,
+  currentPath,
+  route,
+  regex,
+  hidden = false,
+  isMobile = false,
+}) => {
+  if (hidden) {
+    return null;
+  }
+
+  if (isMobile) {
+    return <option value={route}>{children}</option>;
+  }
+
+  let linkClasses =
+    'whitespace-nowrap ml-8 first:ml-0 py-4 px-1 border-b-2 border-transparent font-medium text-sm leading-5';
+  let activeLinkColor =
+    'border-indigo-600 text-indigo-500 focus:outline-none focus:text-indigo-500 focus:border-indigo-500';
+  let inactiveLinkColor =
+    'border-transparent text-gray-500 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-4700 focus:border-gray-300';
+
+  if (tabType === 'button') {
+    linkClasses =
+      'whitespace-nowrap ml-8 first:ml-0 px-3 py-2 font-medium text-sm rounded-md';
+    activeLinkColor = 'bg-indigo-700';
+    inactiveLinkColor = 'bg-gray-800';
+  }
+
+  return (
+    <Link href={route}>
+      <a
+        className={`${linkClasses} ${
+          currentPath.match(regex) ? activeLinkColor : inactiveLinkColor
+        }`}
+        aria-current="page"
+      >
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+const SettingsTabs: React.FC<{
+  tabType?: 'default' | 'button';
+  settingsRoutes: SettingsRoute[];
+  defaultValue?: string;
+}> = ({ tabType = 'default', settingsRoutes, defaultValue }) => {
+  const router = useRouter();
+  const { user: currentUser } = useUser();
+
+  return (
+    <>
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a Tab
+        </label>
+        <select
+          onChange={(e) => {
+            router.push(e.target.value);
+          }}
+          onBlur={(e) => {
+            router.push(e.target.value);
+          }}
+          defaultValue={
+            defaultValue ??
+            settingsRoutes.find((route) => !!router.pathname.match(route.regex))
+              ?.route
+          }
+          aria-label="Selected Tab"
+        >
+          {settingsRoutes
+            .filter(
+              (route) =>
+                !route.hidden &&
+                (route.requiredPermission
+                  ? hasPermission(
+                      route.requiredPermission,
+                      currentUser?.permissions ?? 0,
+                      route.permissionType
+                    )
+                  : true)
+            )
+            .map((route, index) => (
+              <SettingsLink
+                tabType={tabType}
+                currentPath={router.pathname}
+                route={route.route}
+                regex={route.regex}
+                isMobile
+                key={`mobile-settings-link-${index}`}
+              >
+                {route.text}
+                {router.pathname}
+              </SettingsLink>
+            ))}
+        </select>
+      </div>
+      {tabType === 'button' ? (
+        <div className="hidden overflow-x-scroll overflow-y-hidden sm:block hide-scrollbar">
+          <nav className="flex space-x-4" aria-label="Tabs">
+            {settingsRoutes.map((route, index) => (
+              <SettingsLink
+                tabType={tabType}
+                currentPath={router.pathname}
+                route={route.route}
+                regex={route.regex}
+                key={`standard-settings-link-${index}`}
+              >
+                {route.content ?? route.text}
+              </SettingsLink>
+            ))}
+          </nav>
+        </div>
+      ) : (
+        <div className="hidden sm:block">
+          <div className="border-b border-gray-600">
+            <nav className="flex -mb-px">
+              {settingsRoutes
+                .filter(
+                  (route) =>
+                    !route.hidden &&
+                    (route.requiredPermission
+                      ? hasPermission(
+                          route.requiredPermission,
+                          currentUser?.permissions ?? 0,
+                          route.permissionType
+                        )
+                      : true)
+                )
+                .map((route, index) => (
+                  <SettingsLink
+                    tabType={tabType}
+                    currentPath={router.pathname}
+                    route={route.route}
+                    regex={route.regex}
+                    key={`standard-settings-link-${index}`}
+                  >
+                    {route.text}
+                  </SettingsLink>
+                ))}
+            </nav>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SettingsTabs;

src/components/UserProfile/UserSettings/UserNotificationSettings/UserNotificationsDiscord.tsx
@@ -1,0 +1,122 @@
+import axios from 'axios';
+import { Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+import { useToasts } from 'react-toast-notifications';
+import useSWR from 'swr';
+import * as Yup from 'yup';
+import { UserSettingsNotificationsResponse } from '../../../../../server/interfaces/api/userSettingsInterfaces';
+import { useUser } from '../../../../hooks/useUser';
+import globalMessages from '../../../../i18n/globalMessages';
+import Button from '../../../Common/Button';
+import LoadingSpinner from '../../../Common/LoadingSpinner';
+
+const messages = defineMessages({
+  discordsettingssaved: 'Discord notification settings saved successfully!',
+  discordsettingsfailed: 'Discord notification settings failed to save.',
+  discordId: 'User ID',
+  discordIdTip:
+    'The <FindDiscordIdLink>ID number</FindDiscordIdLink> for your user account',
+  validationDiscordId: 'You must provide a valid user ID',
+});
+
+const UserNotificationsDiscord: React.FC = () => {
+  const intl = useIntl();
+  const { addToast } = useToasts();
+  const router = useRouter();
+  const { user } = useUser({ id: Number(router.query.discordId) });
+  const { data, error, revalidate } = useSWR<UserSettingsNotificationsResponse>(
+    user ? `/api/v1/user/${user?.id}/settings/notifications` : null
+  );
+
+  const UserNotificationsDiscordSchema = Yup.object().shape({
+    discordId: Yup.string()
+      .nullable()
+      .matches(/^\d{17,18}$/, intl.formatMessage(messages.validationDiscordId)),
+  });
+
+  if (!data && !error) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        discordId: data?.discordId,
+      }}
+      validationSchema={UserNotificationsDiscordSchema}
+      onSubmit={async (values) => {
+        try {
+          await axios.post(`/api/v1/user/${user?.id}/settings/notifications`, {
+            discordId: values.discordId,
+          });
+          addToast(intl.formatMessage(messages.discordsettingssaved), {
+            appearance: 'success',
+            autoDismiss: true,
+          });
+        } catch (e) {
+          addToast(intl.formatMessage(messages.discordsettingsfailed), {
+            appearance: 'error',
+            autoDismiss: true,
+          });
+        } finally {
+          revalidate();
+        }
+      }}
+    >
+      {({ errors, touched, isSubmitting }) => {
+        return (
+          <Form className="section">
+            <div className="form-row">
+              <label htmlFor="discordId" className="text-label">
+                <span>{intl.formatMessage(messages.discordId)}</span>
+                <span className="label-tip">
+                  {intl.formatMessage(messages.discordIdTip, {
+                    FindDiscordIdLink: function FindDiscordIdLink(msg) {
+                      return (
+                        <a
+                          href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-gray-100 underline transition duration-300 hover:text-white"
+                        >
+                          {msg}
+                        </a>
+                      );
+                    },
+                  })}
+                </span>
+              </label>
+              <div className="form-input">
+                <div className="form-input-field">
+                  <Field id="discordId" name="discordId" type="text" />
+                </div>
+                {errors.discordId && touched.discordId && (
+                  <div className="error">{errors.discordId}</div>
+                )}
+              </div>
+            </div>
+            <div className="actions">
+              <div className="flex justify-end">
+                <span className="inline-flex ml-3 rounded-md shadow-sm">
+                  <Button
+                    buttonType="primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting
+                      ? intl.formatMessage(globalMessages.saving)
+                      : intl.formatMessage(globalMessages.save)}
+                  </Button>
+                </span>
+              </div>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default UserNotificationsDiscord;